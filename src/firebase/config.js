5689a802d4551f5cef68425e061c351b4810c923
Sliver ㅤ‮ㅤ‮ <>
2020-07-03 00:49:53 +0900

    react-ec-site™

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyA_bXJiXrLZyA-yIJ7gviqcreHZCX-3lIY",
+    authDomain: "react-ec-ade24.firebaseapp.com",
+    databaseURL: "https://react-ec-ade24.firebaseio.com",
+    projectId: "react-ec-ade24",
+    storageBucket: "react-ec-ade24.appspot.com",
+    messagingSenderId: "273048558738",
+    appId: "1:273048558738:web:36ea5fbfa4f47824fa5a4b",
+    measurementId: "G-1Z4QEM1PP1"
+};
